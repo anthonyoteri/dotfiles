a5453b9159f26545c3d68ee4734c2894c96f908b
Anthony Oteri <anthony.oteri@gmail.com>
2023-10-24 11:22:17 -0400

    Fix broken man pager display

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 # Hide welcome message & ensure we are reporting fish as shell
 set fish_greeting
 set VIRTUAL_ENV_DISABLE_PROMPT "1"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -pl man'"
 set -x SHELL /usr/bin/fish
 
 # Environment setup
